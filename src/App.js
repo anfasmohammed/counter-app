e6feb69221a3d5e3ff05da3c0495065519dae94f
anfasmohammed <anfasmohammed489@gmail.com>
2024-09-09 15:55:54 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
 
   return (
     <div className="bg-slate-900 flex flex-col gap-9 justify-center items-center h-screen mt-9">
-      <h1 className="text-9xl text-zinc-200">{count}</h1>
+      <h1 className="text-5xl lg:text-9xl text-zinc-200">{count}</h1>
       <div className="flex justify-between gap-3 ">
-      <button onClick={()=>{count===0?setCount(count): setCount(count-1)}} className="rounded-xl font-mono font-semibold bg-gray-100 text-slate-600 px-7 py-2 text-5xl">-</button>
-      <button onClick={reset} className=" font-semibold bg-gray-100 text-slate-600 px-7 py-2 rounded text-5xl">Reset</button>
-      <button onClick={()=>{count===10?setCount(count): setCount(count+1)}} className=" rounded-xl font-semibold bg-gray-100 text-slate-600 px-7 py-2 text-5xl">+</button>
+      <button onClick={()=>{count===0?setCount(count): setCount(count-1)}} className="rounded-xl font-mono font-semibold bg-gray-100 text-slate-600 px-5 lg:px-7 py-2 text-2xl lg:text-5xl">-</button>
+      <button onClick={reset} className=" font-semibold bg-gray-100 text-slate-600 px-5 lg:px-7 py-2 rounded text-2xl lg:text-5xl">Reset</button>
+      <button onClick={()=>{count===10?setCount(count): setCount(count+1)}} className=" rounded-xl font-semibold bg-gray-100 text-slate-600 px-5 lg:px-7 py-2 text-2xl lg:text-5xl">+</button>
       
       
       </div>
