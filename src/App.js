cf8a429fd79825a1aed594d4e526ef4d60c9f4a1
anfasmohammed <163500801+anfasmohammed@users.noreply.github.com>
2024-10-27 22:02:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
 function App() {
+  const [count,setCount]=useState(0)
+ 
+  const reset=()=>{
+    setCount(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-900 flex flex-col gap-9 justify-center items-center h-screen">
+      <h1 className="text-5xl lg:text-9xl text-zinc-200">{count}</h1>
+      <div className="flex justify-between gap-3 ">
+      <button onClick={()=>{count===-100?setCount(count): setCount(count-1)}} className="rounded-xl font-mono font-semibold bg-gray-100 text-slate-600 px-5 lg:px-7 py-2 text-2xl lg:text-5xl">-</button>
+      <button onClick={reset} className=" font-semibold bg-gray-100 text-slate-600 px-5 lg:px-7 py-2 rounded text-2xl lg:text-5xl">Reset</button>
+      <button onClick={()=>{count===100?setCount(count): setCount(count+1)}} className=" rounded-xl font-semibold bg-gray-100 text-slate-600 px-5 lg:px-7 py-2 text-2xl lg:text-5xl">+</button>
+      
+      
+      </div>
     </div>
   );
 }
